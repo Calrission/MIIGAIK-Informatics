3ac54e16056f2a486d2fd27604c207cb3039a27a
artemii <kavrese@gmail.com>
2023-11-02 13:15:46 +0300

    completed Homework_2

---
+++ Python/Hometasks/Homework_2/stack.py
@@ -0,0 +1,21 @@
+class Stack:
+    def __init__(self, *args):
+        self.__data = list(args)
+
+    def push(self, element):
+        self.__data.append(element)
+
+    def pop(self):
+        return self.__data.pop(-1)
+
+    def __str__(self):
+        return ", ".join(list(map(str, self.__data)))
+
+
+if __name__ == "__main__":
+    stack = Stack(10, 20, 30, 40)
+    print(stack.pop())
+    print(stack.pop())
+    print(stack)
+    stack.push("elem")
+    print(stack)
