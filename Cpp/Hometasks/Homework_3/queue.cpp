c0bd2fb5c99a9fccb86f17a28800b9d7dbe75766
artemii <kavrese@gmail.com>
2023-11-02 13:24:16 +0300

    completed Homework_3

--- Cpp/Hometasks/Homework_3/queue.cpp
+++ Cpp/Hometasks/Homework_3/queue.cpp
@@ -1,7 +1,34 @@
 #include "iostream"
+#include "vector"
 
 using namespace std;
 
+vector<int> queue;
+
+void enqueue(int elem){
+    queue.push_back(elem);
+}
+
+int dequeue(){
+    int elem = queue.back();
+    queue.pop_back();
+    return elem;
+}
+
+void show(){
+    for (int& i: queue){
+        cout << i << " ";
+    }
+    cout << endl;
+}
+
 int main(){
-    cout << "test" << endl;
+    enqueue(1);
+    show();
+    enqueue(2);
+    show();
+    enqueue(3);
+    show();
+    cout << dequeue() << endl;
+    show();
 }
