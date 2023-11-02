7bba3a6e54d0d96894165beb24c026dd06b19920
artemii <kavrese@gmail.com>
2023-11-02 11:28:13 +0300

    added not completed hometask_4

---
+++ Cpp/Classworks/Classwork_4/task_1.cpp
@@ -0,0 +1,30 @@
+#include <vector>
+#include <iostream>
+using namespace std;
+
+// Что выведено на экран? (прокомментируйте код)
+
+int main() {
+    vector<int> arr(20, 0);
+    int i = 23;
+    for (auto &x : arr) {
+        x = i++;
+        i *= 33;
+    }
+
+    auto isEven = [](int i) {
+        return (i % 2 == 0);
+    };
+
+    auto any = [isEven](vector<int> &a) {
+        return any_of(a.begin(), a.end(), isEven);
+    };
+
+    auto all = [isEven](vector<int> &a) {
+        return all_of(a.begin(), a.end(), isEven);
+    };
+
+    cout << any(arr) << " " << all(arr);
+
+    return 0;
+}
